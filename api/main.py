947da99d2e76a514d4c00c711b0905cbad4d1057
AnawaratneMANA <it19086408@my.sliit.lk>
2022-04-19 15:11:09 +0530

    audio classification model added.

--- api/main.py
+++ api/main.py
@@ -1,3 +1,8 @@
+import os
+import pathlib
+import shutil
+
+import aiofiles as aiofiles
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
 import numpy as np
@@ -5,10 +10,13 @@ from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 
+from methods.audio_methods import preprocess_dataset, audio_labels, create_upload_file
+
 app = FastAPI()
 
 model_1 = tf.keras.models.load_model("../saved_models/1")
 model_2 = tf.keras.models.load_model("../saved_models/2")
+audio_model = tf.keras.models.load_model("../saved_models/audio_model/audio_model.h5")
 CLASS_NAMES = ['Large ', 'Small', 'Unclear']
 CLASS_NAMES_2 = ['apple1', 'apple2', 'apple3']
 
@@ -37,6 +45,38 @@ async def predict(
         'class': predicted_class,
         'confidence': float(confidence)
     }
+
+@app.post("/audio")
+async def audio_predict(
+    # Save the file.
+    file: UploadFile = File(...)
+):
+    print(await create_upload_file(await file.read())['info'])
+    audio = preprocess_dataset(await file.read())
+    audio_batch = np.expand_dims(audio, 0)
+    predictions = audio_model.predict(audio_batch)
+    predicted_class = audio_labels[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+@app.post("/upload-file/")
+async def create_upload_file(file: UploadFile = File(...)):
+    working_dir = pathlib.Path().absolute()
+    file_location = f"{working_dir}\\..\\temp\\{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+    audio = preprocess_dataset([str(file_location)])
+    for spectrogram, label in audio.batch(1):
+        predictions = audio_model(spectrogram)
+        predicted_class = audio_labels[np.argmax(predictions[0])]
+        confidence = np.max(predictions[0])
+        return {
+            'class': predicted_class,
+            'confidence': float(confidence)
+        }
 
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
