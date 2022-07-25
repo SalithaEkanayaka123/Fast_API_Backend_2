dc11dfce0027f9140a88203d88974f21cd8e5ae4
AnawaratneMANA <it19086408@my.sliit.lk>
2022-07-25 07:31:07 +0530

    COMMIT: Schema added to classification history.

--- db/schema.py
+++ db/schema.py
@@ -6,3 +6,13 @@ class CreateUsers(BaseModel):
 
     class Config:
         orm_mode = True
+
+# Schema class for classification history.
+class CreateClassification(BaseModel):
+    user_id: int
+    classification_category: str
+    classification_name: str
+    classification_label: str
+    confidence_value: str
+    date: str
+
